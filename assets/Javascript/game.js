eb038053198ec19d4e74882c6193a8a54b75e4bd
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-11 22:13:37 -0700

    modified JS

---
+++ assets/Javascript/game.js
@@ -0,0 +1,21 @@
+//set your variables, two arrays, one with the words, second with the blanks
+
+var win = 0;
+var lose = 0;
+var wordList = ["ObiWan Kenobi", "Master Yoda", "Luke Skywalker", "Han Solo"];
+var blankList = ["______ ______", "______ ____", "____ _________", "___ ____"];
+
+
+
+//an on key up function for the game play
+$("document").onkeyup(function(){
+
+
+
+})
+
+//if the letter hit corresponds to a letter in the word, display that letter
+//if they get all the letters of the word correct ++ wins
+//if they use all there guesses ++ lose
+//after a win or a loss render a new word
+//continue to cycle through the words
